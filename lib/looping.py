38c6cf6f719f37655bb0a6ce5a5fb71a30a5bd99
anthonyLuisSotoColling <govips@gmail.com>
2023-07-06 16:27:43 -0700

    squar

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+        print("Happy New Year!")
 def square_integers(int_list):
-    # code goes here!
-    pass
-
+    return [x**2 for x in int_list]
+    
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(100):
+        if ((i+1)%15 == 0):
+            print("FizzBuzz") 
+        elif ((i+1)%5 == 0):
+            print("Buzz")
+        elif ((i+1)%3 == 0):
+            print("Fizz")
+        else:
+            print(i+1)
